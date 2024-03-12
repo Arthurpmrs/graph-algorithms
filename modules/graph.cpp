3a357b78b279c07f74f591bdc639a99b9539129f
BeaRoodriguees <beatrizroodriguees@hotmail.com>
2024-03-12 15:04:09 -0300

    feat: algoritmo de kosaraju e dijkstra

--- modules/graph.cpp
+++ modules/graph.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-Graph::Graph(int n) : _adjList(n + 1), _predecessors(n + 1, -1), _parents(n + 1, -1)
+Graph::Graph(int n) : _adjList(n + 1), _predecessors(n + 1, -1), _parents(n + 1, -1), _postorder(n + 1, -1) , _distances(n + 1, 1e9)
 {
     _visitCount = 0;
-    _vertexCount = n + 1;
+    _visitPostorderCount = 0;
+    _vertexCount = n;
     for (int i = 0; i < _vertexCount; i++)
     {
         _adjList[i] = vector<Edge>();
@@ -16,6 +17,11 @@ void Graph::addEdge(int u, int v, int weight)
 {
     _adjList[u].push_back({v, weight});
     _adjList[v].push_back({u, weight});
+}
+
+// Versão sem peso e direcionado
+void Graph::addEdge2(int u, int v){
+    _adjList[u].push_back({v, -1});
 }
 
 int Graph::getSize()
@@ -33,9 +39,9 @@ void Graph::visit(int v)
     _predecessors[v] = _visitCount++;
 }
 
-void Graph::resetVisited() {
-    _visitCount = 0;
-    _predecessors = vector<int>(_vertexCount, -1);
+void Graph::visitPostorder(int v)
+{
+    _postorder[v] = _visitPostorderCount++;
 }
 
 void Graph::registerParent(int vertex, int parent)
@@ -48,13 +54,26 @@ const vector<Edge> &Graph::getVertexEdges(int v)
     return _adjList[v];
 }
 
-int Graph::getParent(int v) {
-    return _parents[v];
+Edge *Graph::getSmallestWeightEdge(int v, set<int> &exclude)
+{
+    Edge *smallestCostEdge = NULL;
+    int smallestCost = 1000000000;
+    for (int i = 0; i < _adjList[v].size(); ++i)
+    {
+        int neighbor = _adjList[v][i].neighbor;
+        int weight = _adjList[v][i].weight;
+        if (exclude.find(neighbor) != exclude.end() && weight < smallestCost)
+        {
+            smallestCost = weight;
+            smallestCostEdge = &_adjList[v][i];
+        }
+    }
+    return smallestCostEdge;
 }
 
 void Graph::print()
 {
-    for (int i = 0; i < _vertexCount; i++)
+    for (int i = 1; i < _vertexCount+1; i++)
     {
         cout << i << " -> ";
         for (unsigned int j = 0; j < _adjList[i].size(); j++)
@@ -69,9 +88,19 @@ void Graph::print()
 void Graph::printVisited()
 {
     cout << "Visited" << endl;
-    for (int i = 0; i < _vertexCount; i++)
+    for (int i = 1; i < _vertexCount + 1; i++)
     {
         cout << i << " : " << _predecessors[i] << endl;
+    }
+    cout << endl;
+}
+
+void Graph::printVisitedPO()
+{
+    cout << "Post Order Visited" << endl;
+    for (int i = 1; i < _vertexCount + 1; i++)
+    {
+        cout << i << " : " << _postorder[i] << endl;
     }
     cout << endl;
 }
@@ -79,7 +108,7 @@ void Graph::printVisited()
 void Graph::printParents()
 {
     cout << "Parents" << endl;
-    for (int i = 0; i < _vertexCount; i++)
+    for (int i = 1; i < _vertexCount + 1; i++)
     {
         cout << i << " : " << _parents[i] << endl;
     }
--- modules/graph.h
+++ modules/graph.h
@@ -24,21 +24,27 @@ private:
     vector<int> _predecessors;
     vector<int> _parents;
     int _visitCount;
+    int _visitPostorderCount;
     int _vertexCount;
 
 public:
     Graph(int n);
     void addEdge(int u, int v, int weight);
+    void addEdge2(int u, int v);
     int getSize();
     int notVisited(int v);
     void visit(int v);
-    void resetVisited();
+    void visitPostorder(int v);
     void registerParent(int vertex, int parent);
     const vector<Edge> &getVertexEdges(int v);
-    int getParent(int v);
+    Edge *getSmallestWeightEdge(int v, set<int> &exclude);
     void print();
     void printVisited();
     void printParents();
+    void printVisitedPO();
+    
+    vector<int> _distances;
+    vector<int> _postorder;
 };
 
 #endif
