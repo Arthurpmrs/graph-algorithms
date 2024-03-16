7bdfc01fda75a94cccb6fc713b5b4480b0df5723
BeaRoodriguees <beatrizroodriguees@hotmail.com>
2024-03-16 17:40:54 -0300

    Organizando o repositório

--- kosaraju/dfs-kosaraju.cpp
+++ kosaraju/dfs-kosaraju.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+/**
+ * @param graph reference to the graph
+ * @param v the vertex to start the DFS
+ * @param toPrint boolean to print the strongly connected components
+ */
 void DFSRec(Graph &graph, int v, bool toPrint)
 {
     graph.visit(v);
@@ -22,12 +27,18 @@ void DFSRec(Graph &graph, int v, bool toPrint)
     graph.visitPostorder(v);
 }
 
+/**
+ * @brief DFS algorithm that use the postorder vector to find the strongly connected components
+ *
+ * @param graph reference to the graph
+ * @param postorder vector with the postorder of the graph
+ * @param scc number of strongly connected components
+ */
 int DFSPriority(Graph &graph, vector<int> postorder, int scc = 0)
 {
     for (int i = 1; i < graph.getSize(); i++)
     {
         int v = postorder[i];
-        // cout << "teste: " << v << endl;
         if (graph.notVisited(v))
         {
             cout << v << " ";
@@ -40,19 +51,14 @@ int DFSPriority(Graph &graph, vector<int> postorder, int scc = 0)
     return scc;
 }
 
-void DFS(Graph &graph, bool ignoreZero = true)
+/**
+ * @brief Postorder DFS algorithm
+ *
+ * @param graph reference to the graph to find the postorder
+ */
+void DFS(Graph &graph)
 {
-    int firstVertex;
-    if (ignoreZero)
-    {
-        firstVertex = 1;
-    }
-    else
-    {
-        firstVertex = 0;
-    }
-
-    for (int v = firstVertex; v < graph.getSize(); v++)
+    for (int v = 1; v < graph.getSize(); v++)
     {
         if (graph.notVisited(v))
         {
--- kosaraju/kosaraju.cpp
+++ kosaraju/kosaraju.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
+#include "../modules/utils.h"
 #include "../modules/graph.h"
 #include "dfs-kosaraju.cpp"
 
 using namespace std;
 
-void readGraph(Graph &graph, int edgeCount){
-    for(int i = 0; i < edgeCount; i++){
-        int u, v;
-        cin >> u >> v;
-        graph.addEdge2(u, v);
-    }
-}
-
+/**
+ * @brief Function to get the reversed graph
+ *
+ * @param graph reference to the original graph 
+ * @param reversedGraph reference to the graph that will be reversed
+ */
 void reverseGraph(Graph &graph, Graph &reversedGraph){
     for(int i = 0; i < graph.getSize(); i++){
         const vector<Edge> &edges = graph.getVertexEdges(i);
@@ -21,39 +20,65 @@ void reverseGraph(Graph &graph, Graph &reversedGraph){
     }
 }
 
-// Função para ordenar o vetor de postorder por ordem de visitação e invertê-lo
+/**
+ * @brief Function to sort the postorder vector by visitation order and reverse it
+ *
+ * @param postorder reference to the vector that will be sorted and reversed
+ */
 void reversePO(vector<int> &postorder){
+    // To put the vectors in order
     vector<int> aux = postorder; 
-    for(int i = 1; i < postorder.size(); i++){ // Colocar os vetores na ordem
+    for(int i = 1; i < postorder.size(); i++){
         postorder[aux[i]] = i;
     }
+
     aux = postorder;
-    
-    for(int i = 0; i < postorder.size(); i++){ // Inverter o vetor
+
+    // To reverse the vector
+    for(int i = 0; i < postorder.size(); i++){ 
         postorder[i] = aux[aux.size() - i];
     }
 }
 
-int main(){
-    int n, m;
-    cin >> n >> m;
+int main(int argc, char *argv[]){
+    // Check for -h flag
+    bool has_dash_h = getParameterValue((char *)"-h", argc, argv);
+    if (has_dash_h)
+    {
+        std::cout << "Algoritmo de Kosaraju\n"
+                    << "\n"
+                    << "Opções:\n"
+                    << "-h           : mostra o help\n"
+                    << "-f <arquivo> : indica o 'arquivo' que contém o grafo de entrada\n";
 
-    Graph graph(n);
-    readGraph(graph, m);
-    // graph.print();
+        // If the help flag is used, do not execute the code.
+        return 0;
+    }
 
-    Graph reversedGraph(n);
+    // Check for a -f flag
+    string filename = "";
+    getParameterValue((char *)"-f", argc, argv, &filename);
+
+    // It is mandatory to have a filename
+    if (filename == "")
+    {
+        cout << "Please, enter a valid input file." << endl;
+        return 1;
+    }
+
+    // Create the graph from the file
+    Graph graph = createGraphFromFile2(filename);
+
+    // Create the reversed graph
+    Graph reversedGraph(graph.getSize() - 1);
     reverseGraph(graph, reversedGraph);
-    // reversedGraph.print();
+
+    // Run the DFS on the reversed graph to get the postorder
     DFS(reversedGraph);
+    reversePO(reversedGraph._postorder);
 
-    // reversedGraph.printVisitedPO();
-    reversePO(reversedGraph._postorder); // Utilizar o _postorder com i = 1 ate size;
-    // cout << "Reverse ";
-    // reversedGraph.printVisitedPO();
-
+    // Run the DFS on the original graph using the postorder from the reversed graph
     int scc = DFSPriority(graph, reversedGraph._postorder);
-    // cout << endl << "scc: " << scc << endl;
 
     return 0;
 }
