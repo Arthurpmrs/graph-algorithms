2a3d06869a8ccc1a2c468f5b9950bc13395080c6
BeaRoodriguees <beatrizroodriguees@hotmail.com>
2024-03-16 00:02:09 -0300

    starting bellmanFord

--- bellman/bellman.cpp
+++ bellman/bellman.cpp
@@ -3,93 +3,36 @@
 #include "../modules/graph.h"
 #include "../modules/utils.h"
 
+#define INF 1e9
 
+using namespace std;
 
-/**
- * @brief Writes the minimum spanning tree edges to a file.
- *
- * @param mst reference to the graph that represents the minimum spanning tree
- * @param output_filename name of the file to write the edges
- */
-void writeDistances(Graph &graph, string output_filename)
-{
-    ofstream file(output_filename);
-    for (int i = 1; i < graph.getSize(); i++)
-    {
-        file << i << ":" << graph._distances[i] << " ";
-    }
+void bellmanFord(Graph &graph, int s){
+    graph._distances[s] = 0;
 
-    file << endl;
-
-    file.close();
+    
 }
 
-int main(int argc, char *argv[])
-{
-    // check for -h flag
-    bool has_dash_h = getParameterValue((char *)"-h", argc, argv);
-    if (has_dash_h)
-    {
-        std::cout << "Algoritmo de Dijkstra\n"
-                    << "\n"
-                    << "Opções:\n"
-                    << "-h           : mostra o help\n"
-                    << "-o <arquivo> : redireciona a saida para o 'arquivo'\n"
-                    << "-f <arquivo> : indica o 'arquivo' que contém o grafo de entrada\n"
-                    << "-i           : vértice inicial\n";
-
-        // If the help flag is used, do not execute the code.
-        return 0;
+void readGraph(Graph &graph, int edgeCount){
+    for(int i = 0; i < edgeCount; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph.addEdge(u, v, w);
     }
+}
 
-    // Check for a -f flag
-    string filename = "";
-    getParameterValue((char *)"-f", argc, argv, &filename);
+int main() {
+    int n, m;
+    cin >> n >> m;
 
-    // It is mandatory to have a filename
-    if (filename == "")
-    {
-        cout << "Please, enter a valid input file." << endl;
-        return 1;
+    Graph graph(n);
+    readGraph(graph, m);
+
+    bellmanFord(graph, 1);
+
+    for(int i = 1; i <= n; i++){
+        cout << graph._distances[i] << " ";
     }
+    cout << endl;
 
-    Graph graph = createGraphFromFile(filename);
-
-    string initialVertexStr;
-    getParameterValue((char *)"-i", argc, argv, &initialVertexStr);
-
-    // check if the value is a number.
-    int initialVertex = 0;
-    if (initialVertexStr == "")
-    {
-        initialVertex = 1;
-    }
-    else
-    {
-        try
-        {
-            initialVertex = std::stoi(initialVertexStr);
-        }
-        catch (std::invalid_argument &e)
-        {
-            std::cerr << "Invalid argument: " << initialVertexStr << " is not a number.\n";
-            return 1; // Return with error
-        }
-    }
-
-    // Run the algorithm
-    
-
-    // check for a -o flag
-    string output_filename;
-    bool has_dash_o = getParameterValue((char *)"-o", argc, argv, &output_filename);
-
-    if (has_dash_o)
-    {
-        writeDistances(graph, output_filename);
-    }
-
-
-
-    return 0;
 }
